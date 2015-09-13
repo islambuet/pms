4e4ecd33981e88de041fbcb657d2fdae856558e3
islambuet <islambuet@gmail.com>
2015-09-14 01:10:51 +0600

    Root_controller.php deleted

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Home extends Root_Controller
+class Home extends Root_controller
 {
 	public function index($module_id=0)
 	{
