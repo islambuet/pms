b5c3d1229a6c88cde94ae0f03c26371d38db39ed
islambuet <islambuet@gmail.com>
2015-10-22 02:29:38 +0600

    Delivery container started

---
+++ application/controllers/Delivery_container_allocation.php
@@ -0,0 +1,210 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Delivery_container_allocation extends Root_Controller
+{
+    private  $message;
+    public $permissions;
+    public $controller_url;
+    public $parent_module_id=0;
+    public function __construct()
+    {
+        parent::__construct();
+        $this->message="";
+        $this->permissions=User_helper::get_permission('Delivery_container_allocation');
+        if(isset($this->permissions['task_id'])&&($this->permissions['task_id']>0))
+        {
+            $this->parent_module_id=System_helper::get_parent_id_of_task($this->permissions['task_id']);
+        }
+        $this->controller_url='delivery_container_allocation';
+        //$this->load->model("allocation_consignment_model");
+    }
+
+    public function index($action="search",$id=0)
+    {
+        if($action=="search")
+        {
+            $this->system_search();
+        }
+        elseif($action=="list")
+        {
+            $this->system_list();
+        }
+        elseif($action=="edit")
+        {
+            $this->system_edit();
+        }
+        elseif($action=="save")
+        {
+            $this->system_save();
+        }
+        else
+        {
+            $this->system_search();
+        }
+    }
+
+    public function system_search()
+    {
+
+        if(isset($this->permissions['view'])&&($this->permissions['view']==1))
+        {
+            $data['title']="Assign Quantity";
+            $data['consignments']=array();
+            $ajax['system_content'][]=array("id"=>"#system_content","html"=>$this->load->view("delivery_container_allocation/search",$data,true));
+            if($this->message)
+            {
+                $ajax['system_message']=$this->message;
+            }
+            $ajax['system_page_url']=site_url($this->controller_url.'/index/search');
+            $this->jsonReturn($ajax);
+        }
+        else
+        {
+            $ajax['system_message']=$this->lang->line("YOU_DONT_HAVE_ACCESS");
+            $this->jsonReturn($ajax);
+        }
+    }
+    public function system_list()
+    {
+        if(isset($this->permissions['edit'])&&($this->permissions['edit']==1))
+        {
+            $year=$this->input->post('year');
+            $consignment_id=$this->input->post('consignment_id');
+            $ajax['status']=false;
+            $ajax['system_message']='Under Construction';
+            $this->jsonReturn($ajax);
+
+            $data['title']='Allocation list';
+            $data['bookings']=$this->allocation_consignment_model->get_bookings($year);
+            if(!(sizeof($data['bookings'])>0))
+            {
+                $ajax['status']=false;
+                $ajax['system_message']=$this->lang->line("MSG_NO_PERMANENT_BOOKING_FOUND");
+                $this->jsonReturn($ajax);
+            }
+            $data['consignments']=$this->allocation_consignment_model->get_consignments($year);
+            $data['allocated_varieties']=$this->allocation_consignment_model->get_all_allocated_varieties($year);
+            if(!(sizeof($data['consignments'])>0))
+            {
+                $ajax['status']=false;
+                $ajax['system_message']=$this->lang->line("MSG_NO_CONSIGNMENT_FOUND");
+                $this->jsonReturn($ajax);
+            }
+            $data['colors']=Query_helper::get_info($this->config->item('table_quantity_color'),'*',array());
+            $ajax['system_content'][]=array("id"=>"#detail_container","html"=>$this->load->view("allocation_consignment/list",$data,true));
+
+            $ajax['status']=false;
+            if($this->message)
+            {
+                $ajax['system_message']=$this->message;
+            }
+            $this->jsonReturn($ajax);
+        }
+        else
+        {
+            $ajax['status']=false;
+            $ajax['system_message']=$this->lang->line("YOU_DONT_HAVE_ACCESS");
+            $this->jsonReturn($ajax);
+        }
+    }
+    public function system_edit()
+    {
+        $year=$this->input->post('year');
+        $booking_id=$this->input->post('booking_id');
+        //$consignment_id=$this->input->post('consignment_id');
+        $data['consignment_info']=$this->allocation_consignment_model->get_consignments($year);
+        $data['booking_info']=$this->allocation_consignment_model->get_booking_info($booking_id);
+        $data['allocated_varieties']=$this->allocation_consignment_model->get_allocated_varieties($year,$booking_id);
+        $data['year']=$year;
+        //$data['consignment_id']=$consignment_id;
+        $data['booking_id']=$booking_id;
+        $ajax['system_content'][]=array("id"=>"#edit_container","html"=>$this->load->view("allocation_consignment/edit",$data,true));
+
+        $ajax['status']=false;
+        if($this->message)
+        {
+            $ajax['system_message']=$this->message;
+        }
+        $this->jsonReturn($ajax);
+    }
+
+    public function system_save()
+    {
+        $user=User_helper::get_user();
+
+
+        if(!(isset($this->permissions['edit'])&&($this->permissions['edit']==1)))
+        {
+            $ajax['status']=false;
+            $ajax['system_message']=$this->lang->line("YOU_DONT_HAVE_ACCESS");
+            $this->jsonReturn($ajax);
+            die();
+        }
+
+        if(!$this->check_validation())
+        {
+            $ajax['status']=false;
+            $ajax['system_message']=$this->message;
+            $this->jsonReturn($ajax);
+        }
+        else
+        {
+            $year=$this->input->post('year');
+            $booking_id=$this->input->post('booking_id');
+            //$consignment_id=$this->input->post('consignment_id');
+            $allocated_varieties=$this->input->post('allocated_varieties');
+
+            $time=time();
+            $this->db->trans_start();  //DB Transaction Handle START
+
+            $this->db->where('year',$year);
+            $this->db->where('booking_id',$booking_id);
+            //$this->db->where('consignment_id',$consignment_id);
+            $this->db->set('revision', 'revision+1', FALSE);
+            $this->db->update($this->config->item('table_allocation_varieties'));
+            foreach($allocated_varieties as $consignment)
+            {
+                foreach($consignment as $data)
+                {
+                    $data['date']=System_helper::get_time($data['date']);
+                    $data['revision']=1;
+                    $data['created_by'] = $user->user_id;
+                    $data['creation_date'] = $time;
+                    $data['year'] = $year;
+                    $data['booking_id'] = $booking_id;
+                    Query_helper::add($this->config->item('table_allocation_varieties'),$data);
+                }
+            }
+
+            $this->db->trans_complete();   //DB Transaction Handle END
+
+            if ($this->db->trans_status() === TRUE)
+            {
+                $this->message=$this->lang->line("MSG_SAVED_SUCCESS");
+                $this->system_list();
+            }
+            else
+            {
+                $ajax['status']=false;
+                $ajax['system_message']=$this->lang->line("MSG_SAVED_FAIL");
+                $this->jsonReturn($ajax);
+
+            }
+
+        }
+    }
+    private function check_validation()
+    {
+        $allocated_varieties=$this->input->post('allocated_varieties');
+
+        if(!(sizeof($allocated_varieties)>0))
+        {
+            $this->message="invalid input";
+            return false;
+        }
+
+
+        return true;
+    }
+
+}
---
+++ application/views/delivery_container_allocation/edit.php
@@ -0,0 +1,81 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+$CI = & get_instance();
+//echo '<PRE>';
+//print_r($consignment_info);
+//echo '</PRE>';
+//return;
+//echo '<PRE>';
+//print_r(($booking_info));
+//echo '</PRE>';
+//echo '<PRE>';
+//print_r($allocated_varieties);
+//echo '</PRE>';
+//return;
+?>
+
+
+
+<form class="form_valid" id="save_form" action="<?php echo site_url($CI->controller_url.'/index/save');?>" method="post">
+
+    <input type="hidden" name="year" value="<?php echo $year; ?>" />
+    <input type="hidden" name="booking_id" value="<?php echo $booking_id; ?>" />
+    <?php
+    foreach($consignment_info as $consignment)
+    {
+        ?>
+        <div class="widget-header">
+            <div class="title">
+                <?php echo $consignment['consignment_name']; ?>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+        <table class="table table-bordered">
+            <thead>
+            <tr>
+                <th>Variety</th>
+                <th>Consignment Quantity</th>
+                <th>Customer Quantity</th>
+                <th>Allocated Quantity</th>
+                <th>Date</th>
+            </tr>
+            <tbody>
+            <?php
+            foreach($consignment['varieties'] as $variety)
+            {
+                if(array_key_exists($variety['id'],$booking_info['varieties']))
+                {
+                    ?>
+
+                    <tr>
+                        <td>
+                            <?php echo $variety['variety_name'] ?>
+                            <input type="hidden" name="allocated_varieties[<?php echo $consignment['consignment_id'];?>][<?php echo $variety['id'];?>][variety_id]" value="<?php echo $variety['id']; ?>">
+                            <input type="hidden" name="allocated_varieties[<?php echo $consignment['consignment_id'];?>][<?php echo $variety['id'];?>][consignment_id]" value="<?php echo $consignment['consignment_id']; ?>">
+                        </td>
+                        <td><?php echo number_format($variety['quantity']); ?></td>
+                        <td><?php echo number_format($booking_info['varieties'][$variety['id']]['quantity']); ?></td>
+                        <td><input type="text" name="allocated_varieties[<?php echo $consignment['consignment_id'];?>][<?php echo $variety['id'];?>][quantity]" class="form-control" value="<?php if(isset($allocated_varieties[$consignment['consignment_id']][$variety['id']]['quantity'])){echo $allocated_varieties[$consignment['consignment_id']][$variety['id']]['quantity'];}else{echo '0';} ?>"/></td>
+                        <td><input type="text" name="allocated_varieties[<?php echo $consignment['consignment_id'];?>][<?php echo $variety['id'];?>][date]" class="form-control datepicker" value="<?php if(isset($allocated_varieties[$consignment['consignment_id']][$variety['id']]['date'])){echo System_helper::display_date($allocated_varieties[$consignment['consignment_id']][$variety['id']]['date']);}else{echo System_helper::display_date(time());} ?>"/></td>
+                    </tr>
+                <?php
+                }
+            }
+            ?>
+            </tbody>
+            </thead>
+
+        </table>
+        <?php
+    }
+
+    ?>
+
+</form>
+
+<script type="text/javascript">
+
+    jQuery(document).ready(function()
+    {
+        $(".datepicker").datepicker({dateFormat : display_date_format});
+    });
+</script>
