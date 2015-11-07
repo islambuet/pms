75035f4ac8db82f0c1938ad2ec3a5c5f99dc3480
islambuet <islambuet@gmail.com>
2015-11-07 13:47:01 +0600

    rnd_group changed to pms_group

--- application/models/Dashboard_model.php
+++ application/models/Dashboard_model.php
@@ -17,7 +17,7 @@ class Dashboard_model extends CI_Model {
         $this->db->from($CI->config->item('table_task')." task");
         $this->db->join($CI->config->item('table_user_group_role').' ugr','task.id = ugr.task_id','INNER');
         $this->db->where("task.parent",$module_id);
-        $this->db->where("ugr.user_group_id",$user->rnd_group);
+        $this->db->where("ugr.user_group_id",$user->pms_group);
         $this->db->order_by('task.ordering');
         $result=$this->db->get()->result_array();
         return $result;
--- application/models/Root_model.php
+++ application/models/Root_model.php
@@ -17,7 +17,7 @@ class Root_model extends CI_Model {
         $this->db->from($CI->config->item('table_user_group_role').' ugr');
         $this->db->join($CI->config->item('table_task').' task','task.id = ugr.task_id','INNER');
         $this->db->join($CI->config->item('table_task').' module','module.id = task.parent','INNER');
-        $this->db->where('ugr.user_group_id',$user->rnd_group);
+        $this->db->where('ugr.user_group_id',$user->pms_group);
         $this->db->where('ugr.view',1);
         $this->db->group_by('module.id');
         $this->db->order_by('module.ordering','ASC');
