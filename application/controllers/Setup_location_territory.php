c0969e32f36b464c684aeb16e1cae8a9db7fb916
islambuet <islambuet@gmail.com>
2016-07-03 16:11:41 +0600

    Location Territory Completed

---
+++ application/controllers/Setup_location_territory.php
@@ -0,0 +1,220 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Setup_location_territory extends Root_Controller
+{
+    private  $message;
+    public $permissions;
+    public $controller_url;
+    public function __construct()
+    {
+        parent::__construct();
+        $this->message="";
+        $this->permissions=User_helper::get_permission('Setup_location_territory');
+        $this->controller_url='setup_location_territory';
+    }
+
+    public function index($action="list",$id=0)
+    {
+        if($action=="list")
+        {
+            $this->system_list($id);
+        }
+        elseif($action=="get_items")
+        {
+            $this->get_items();
+        }
+        elseif($action=="add")
+        {
+            $this->system_add();
+        }
+        elseif($action=="edit")
+        {
+            $this->system_edit($id);
+        }
+        elseif($action=="save")
+        {
+            $this->system_save();
+        }
+        else
+        {
+            $this->system_list($id);
+        }
+    }
+
+    private function system_list()
+    {
+        if(isset($this->permissions['view'])&&($this->permissions['view']==1))
+        {
+            $data['title']="Territory List";
+            $ajax['status']=true;
+            $ajax['system_content'][]=array("id"=>"#system_content","html"=>$this->load->view("setup_location_territory/list",$data,true));
+            if($this->message)
+            {
+                $ajax['system_message']=$this->message;
+            }
+            $ajax['system_page_url']=site_url($this->controller_url);
+            $this->jsonReturn($ajax);
+        }
+        else
+        {
+            $ajax['system_message']=$this->lang->line("YOU_DONT_HAVE_ACCESS");
+            $this->jsonReturn($ajax);
+        }
+    }
+    private function get_items()
+    {
+        $this->db->from($this->config->item('table_location_territories').' territories');
+        $this->db->select('territories.id id,territories.name territory_name');
+        $this->db->select('territories.remarks remarks,territories.status status,territories.ordering ordering');
+        $this->db->select('zones.name zone_name');
+        $this->db->join($this->config->item('table_location_zones').' zones','zones.id = territories.zone_id','INNER');
+        $this->db->where('territories.status !=',$this->config->item('system_status_delete'));
+        $items=$this->db->get()->result_array();
+        $this->jsonReturn($items);
+    }
+
+    private function system_add()
+    {
+        if(isset($this->permissions['add'])&&($this->permissions['add']==1))
+        {
+            $data['title']="Create New Territory";
+            $data['territory']['id']=0;
+            $data['territory']['zone_id']=0;
+            $data['territory']['name']='';
+            $data['territory']['remarks']='';
+            $data['territory']['ordering']=99;
+            $data['territory']['status']=$this->config->item('system_status_active');
+            $data['zones']=Query_helper::get_info($this->config->item('table_location_zones'),array('id value','name text'),array('status ="'.$this->config->item('system_status_active').'"'));
+            $ajax['status']=true;
+            $ajax['system_content'][]=array("id"=>"#system_content","html"=>$this->load->view("setup_location_territory/add_edit",$data,true));
+            if($this->message)
+            {
+                $ajax['system_message']=$this->message;
+            }
+            $ajax['system_page_url']=site_url($this->controller_url.'/index/add');
+            $this->jsonReturn($ajax);
+        }
+        else
+        {
+            $ajax['system_message']=$this->lang->line("YOU_DONT_HAVE_ACCESS");
+            $this->jsonReturn($ajax);
+        }
+    }
+    private function system_edit($id)
+    {
+        if(isset($this->permissions['edit'])&&($this->permissions['edit']==1))
+        {
+            if(($this->input->post('id')))
+            {
+                $territory_id=$this->input->post('id');
+            }
+            else
+            {
+                $territory_id=$id;
+            }
+
+            $data['territory']=Query_helper::get_info($this->config->item('table_location_territories'),'*',array('id ='.$territory_id),1);
+            $data['zones']=Query_helper::get_info($this->config->item('table_location_zones'),array('id value','name text'),array('status ="'.$this->config->item('system_status_active').'"'));
+            $data['title']="Edit Territory (".$data['territory']['name'].')';
+            $ajax['status']=true;
+            $ajax['system_content'][]=array("id"=>"#system_content","html"=>$this->load->view("setup_location_territory/add_edit",$data,true));
+            if($this->message)
+            {
+                $ajax['system_message']=$this->message;
+            }
+            $ajax['system_page_url']=site_url($this->controller_url.'/index/edit/'.$territory_id);
+            $this->jsonReturn($ajax);
+        }
+        else
+        {
+            $ajax['system_message']=$this->lang->line("YOU_DONT_HAVE_ACCESS");
+            $this->jsonReturn($ajax);
+        }
+    }
+
+    private function system_save()
+    {
+        $user=User_helper::get_user();
+        $time=time();
+        $id = $this->input->post("id");
+        if($id>0)
+        {
+            if(!(isset($this->permissions['edit'])&&($this->permissions['edit']==1)))
+            {
+                $ajax['status']=false;
+                $ajax['system_message']=$this->lang->line("YOU_DONT_HAVE_ACCESS");
+                $this->jsonReturn($ajax);
+                die();
+            }
+        }
+        else
+        {
+            if(!(isset($this->permissions['add'])&&($this->permissions['add']==1)))
+            {
+                $ajax['status']=false;
+                $ajax['system_message']=$this->lang->line("YOU_DONT_HAVE_ACCESS");
+                $this->jsonReturn($ajax);
+                die();
+
+            }
+        }
+        if(!$this->check_validation())
+        {
+            $ajax['status']=false;
+            $ajax['system_message']=$this->message;
+            $this->jsonReturn($ajax);
+        }
+        else
+        {
+            $data = $this->input->post('territory');
+            $this->db->trans_start();  //DB Transaction Handle START
+            if($id>0)
+            {
+                $data['user_updated']=$user->user_id;
+                $data['date_updated']=$time;
+                Query_helper::update($this->config->item('table_location_territories'),$data,array("id = ".$id));
+            }
+            else
+            {
+                $data['user_created'] = $user->user_id;
+                $data['date_created'] = $time;
+                Query_helper::add($this->config->item('table_location_territories'),$data);
+            }
+            $this->db->trans_complete();   //DB Transaction Handle END
+            if ($this->db->trans_status() === TRUE)
+            {
+                $save_and_new=$this->input->post('system_save_new_status');
+                $this->message=$this->lang->line("MSG_SAVED_SUCCESS");
+                if($save_and_new==1)
+                {
+                    $this->system_add();
+                }
+                else
+                {
+                    $this->system_list();
+                }
+            }
+            else
+            {
+                $ajax['status']=false;
+                $ajax['system_message']=$this->lang->line("MSG_SAVED_FAIL");
+                $this->jsonReturn($ajax);
+            }
+        }
+    }
+    private function check_validation()
+    {
+        $this->load->library('form_validation');
+        $this->form_validation->set_rules('territory[name]',$this->lang->line('LABEL_TERRITORY_NAME'),'required');
+        $this->form_validation->set_rules('territory[zone_id]',$this->lang->line('LABEL_ZONE_NAME'),'required');
+
+        if($this->form_validation->run() == FALSE)
+        {
+            $this->message=validation_errors();
+            return false;
+        }
+        return true;
+    }
+
+
+}
